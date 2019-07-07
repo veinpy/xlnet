ded4f6943d50bcc54beb1f9de7816878daab9c22
veinpy <vein@veins-MacBook.local>
2019-07-08 02:44:36 +0800

    ..

--- src/spm_gen.py
+++ src/spm_gen.py
@@ -13,6 +13,7 @@ spm.SentencePieceTrainer.Train("""--input=spm_test.txt \
         --vocab_size=100000 \
         --control_symbols=<cls>,<sep>,<pad>,<mask>,<eod> \
         --model_type=unigram \
+        --use_all_vocab true
         --user_defined_symbols=<eop>,.,(,),\",-,–,£,€""")
 
 
@@ -23,3 +24,6 @@ sp.encode_as_ids("""我不能确定对方是不是喜欢我，我却想分分秒
 一定要告诉他你很喜欢他 很爱他!!  虽然不知道你和他现在的关系是什么！但如果真的觉得很喜欢就向他表白啊！！起码你努力过了！  女生主动多少占一点优势的！！呵呵  只愿曾经拥有！  到以后就算感情没现在这么强烈了也不会觉得遗憾啊~！  与其每天那么痛苦的想他 恋他 还不如直接告诉他 ！  不要怕回破坏你们现有的感情！因为如果不告诉他  你可能回后悔一辈子！！  
 
 """)
+
+
+
